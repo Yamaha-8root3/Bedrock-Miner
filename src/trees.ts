56551ce9c8245fe2f1de65ba6ca3173f745dd921
yamaha-8root3 <128972529+Yamaha-8root3@users.noreply.github.com>
2024-02-15 22:38:13 +0900

    TreeProviderとTreeItemをリファレンス通りに書き直し

--- src/trees.ts
+++ src/trees.ts
@@ -3,7 +3,7 @@ import { ProjectListProvider } from "./trees/projectList";
 
 export async function registerExtensionTrees(): Promise<void> {
   return new Promise(() => {
-    vscode.window.registerTreeDataProvider("bm-menu-projects", new ProjectListProvider());
+    vscode.window.registerTreeDataProvider("bm-menu-projects", new ProjectListProvider(undefined));
     console.log("Bedrock Miner > Trees Registered");
   });
 }
--- src/trees/projectList.ts
+++ src/trees/projectList.ts
@@ -1,54 +1,31 @@
 import * as vscode from "vscode";
 
-export class ProjectListProvider implements vscode.TreeDataProvider<ProjectListElement> {
-  private rootElements: ProjectListElement[];
-  constructor() {
-    this.rootElements = this.createElements();
+export class ProjectListProvider implements vscode.TreeDataProvider<Project> {
+  constructor(private projectFolder: string | undefined) {}
+  getTreeItem(element: Project): vscode.TreeItem {
+    return element;
   }
-  getTreeItem(element: ProjectListElement): vscode.TreeItem {
-    const collapsibleState =
-      element.children.length > 0
-        ? vscode.TreeItemCollapsibleState.Collapsed
-        : vscode.TreeItemCollapsibleState.None;
-    return new vscode.TreeItem(element.name, collapsibleState);
-  }
-
-  getChildren(element?: ProjectListElement): vscode.ProviderResult<ProjectListElement[]> {
-    return element ? element.children : this.rootElements;
-  }
-
-  createElements(): ProjectListElement[] {
-    const projects = new ProjectListElement("Projects");
-    projects.addChild(new ProjectListElement("test"));
-    return [projects];
+  getChildren(element?: Project): Thenable<Project[]> {
+    if (!element) {
+      return Promise.resolve([
+        new Project("Add Project", vscode.TreeItemCollapsibleState.None, "add"),
+        new Project("List", vscode.TreeItemCollapsibleState.Collapsed, "folder"),
+      ]);
+    } else {
+      return Promise.resolve([]);
+    }
   }
 }
 
-export class ProjectListElement {
-  private _children: ProjectListElement[];
-  private _parent: ProjectListElement | undefined | null;
-  constructor(public name: string) {
-    this._children = [];
-  }
-  get parent(): ProjectListElement | undefined | null {
-    return this._parent;
-  }
-
-  get children(): ProjectListElement[] {
-    return this._children;
-  }
-
-  addChild(child: ProjectListElement) {
-    child.parent?.removeChild(child);
-    this._children.push(child);
-    child._parent = this;
-  }
-
-  removeChild(child: ProjectListElement) {
-    const childIndex = this._children.indexOf(child);
-    if (childIndex >= 0) {
-      this._children.splice(childIndex, 1);
-      child._parent = null;
-    }
+export class Project extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly collapsibleState?: vscode.TreeItemCollapsibleState,
+    public readonly icon?: string
+  ) {
+    super(label, collapsibleState ? collapsibleState : vscode.TreeItemCollapsibleState.None);
+    this.iconPath = new vscode.ThemeIcon(icon ? icon : "project");
+    // this.description = "description";
+    // this.tooltip = "tooltip";
   }
 }
